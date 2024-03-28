b04f38869a045c0d17da2b2d33fc1e1b42f8c944
mark_ydh <2247654142@qq.com>
2024-03-28 18:00:04 +0800

    引入新图标 以及更新主页界面

--- mark-blog/src/views/IndexView/Index.vue
+++ mark-blog/src/views/IndexView/Index.vue
@@ -17,14 +17,27 @@
       <p>springboot</p>
       <p>lombok</p>
       <div style="display: inline-flex;justify-content: flex-start">
-        <a href="https://github.com/Markydh" class="iconfont" style="font-size: 30px;padding-left: 5px">&#xe709;</a>
-        <a href="https://mail.google.com" class="iconfont" style="font-size: 35px;padding-left: 8px">&#xe67c;</a>
-        <a href="https://www.instagram.com/" class="iconfont" style="font-size: 35px;padding-left: 8px">&#xe736;</a>
-        <a href="https://discord.com/channels/@me" class="iconfont" style="font-size: 35px;padding-left: 8px">&#xe618;</a>
+        <a href="https://github.com/Markydh" class="iconfont" :id="github" @mouseover="hoverEvent('github')" @mouseleave="hoverEvent('github')">&#xe709;</a>
+        <a href="https://mail.google.com" class="iconfont" :id="gmail" @mouseover="hoverEvent('gmail')" @mouseleave="hoverEvent('gmail')">&#xe67c;</a>
+        <a href="https://www.instagram.com/" class="iconfont" :id="ins" @mouseover="hoverEvent('ins')" @mouseleave="hoverEvent('ins')">&#xe736;</a>
+        <a href="https://discord.com/channels/@me" class="iconfont" :id="discord" @mouseover="hoverEvent('discord')" @mouseleave="hoverEvent('discord')">&#xe618;</a>
       </div>
+      <hr style="border: 1px solid rgba(178,178,178,0.31); margin-top: 50px">
     </div>
-    <div class="test1">
 
+    <div class="my-project">
+      <h1>💻我的项目</h1>
+      <div class="project-content">
+        <div :class="projectDetail" @mouseenter="hoverEvent('projectDetail')" @mouseleave="hoverEvent('projectDetail')">
+            <div class="project-name" >
+              <i class="iconfont" style="font-size: 27px;color: #60bddc">&#xe799;</i>
+            </div>
+          <div class="project-itd">
+          </div>
+          <div class="project-tags">
+          </div>
+        </div>
+      </div>
     </div>
   </div>
   </div>
@@ -32,7 +45,54 @@
 
 <script>
 export default {
-  name: "Index.vue"
+  name: "Index.vue",
+  data(){
+    return {
+      github:'github-nonActive',
+      gmail:'gmail-nonActive',
+      ins:'ins-nonActive',
+      discord:'discord-nonActive',
+      projectDetail: 'project-detail-nonActive'
+    }
+  },
+  methods:{
+    hoverEvent(name){
+      if(name=='github') {
+        if (this.github == 'github-nonActive')
+          this.github = 'github-Active'
+        else {
+          this.github = 'github-nonActive'
+        }
+      }
+      else if(name=='gmail'){
+        if (this.gmail == 'gmail-nonActive')
+          this.gmail = 'gmail-Active'
+        else {
+          this.gmail = 'gmail-nonActive'
+        }
+      }
+      else if(name=='ins'){
+        if (this.ins == 'ins-nonActive')
+          this.ins = 'ins-Active'
+        else {
+          this.ins = 'ins-nonActive'
+        }
+      }
+      else if(name=='discord'){
+        if (this.discord == 'discord-nonActive')
+          this.discord = 'discord-Active'
+        else {
+          this.discord = 'discord-nonActive'
+        }
+      }else{
+        if (this.projectDetail == 'project-detail-nonActive')
+          this.projectDetail = 'project-detail-Active'
+        else {
+          this.projectDetail = 'project-detail-nonActive'
+        }
+      }
+    }
+  }
 }
 </script>
 
@@ -51,11 +111,113 @@ export default {
   width:100%;
   height: auto;
 }
+
+.my-project{
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+}
+
+.project-content{
+  width: 100%;
+  height: 300px;
+  display: grid;
+  grid-template-columns: repeat(2,50%);
+  grid-template-rows: repeat(3,50%);
+  column-gap: 20px;
+  row-gap: 20px;
+}
+
+
+/*项目界面悬浮效果*/
+.project-detail-nonActive{
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  background-color: white;
+  border: 2px  solid rgba(103, 103, 103, 0.76);
+  border-radius:4px;
+}
+
+
+.project-detail-Active{
+  display: flex;
+  justify-content: flex-start;
+  background-color: rgba(129, 129, 129, 0.32);
+  flex-direction: column;
+  border: 2px  solid rgba(103, 103, 103, 0.76);
+  border-radius:4px;
+}
+
+
+.project-name{
+  padding-left: 20px;
+  padding-top: 20px;
+}
+.project-itd{
+  padding-left: 20px;
+  padding-top: 20px;
+}
+.project-tags{
+  padding-left: 20px;
+  padding-top: 20px;
+}
+
 .p{
   font-family: "Hiragino Sans GB";
 }
 a{
   text-decoration: none;
   color: #000;
+}
+
+/*鼠标悬浮github图标事件*/
+#github-nonActive{
+  color: #000;
+  font-size: 33px;
+  padding-left: 8px
+}
+#github-Active{
+  color: #b22ac0;
+  font-size: 33px;
+  padding-left: 8px
+}
+
+/*鼠标悬浮gmail图标事件*/
+#gmail-nonActive{
+  color: #000;
+  font-size: 35px;
+  padding-left: 8px
+}
+#gmail-Active{
+  color: #15d959;
+  font-size: 35px;
+  padding-left: 8px
+}
+
+/*鼠标悬浮ins图标事件*/
+#ins-nonActive{
+  color: #000;
+  font-size: 35px;
+  padding-left: 8px
+}
+#ins-Active{
+  color: #ea1186;
+  font-size: 35px;
+  padding-left: 8px
+}
+
+
+/*鼠标悬浮discord图标事件*/
+#discord-nonActive{
+  color: #000;
+  font-size: 35px;
+  padding-left: 8px
+}
+#discord-Active{
+  color: #1b75da;
+  font-size: 35px;
+  padding-left: 8px
 }
 </style>
